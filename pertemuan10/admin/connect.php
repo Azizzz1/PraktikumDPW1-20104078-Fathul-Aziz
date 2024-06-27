b6918b4b407e89e721aa4b059ad24cd94e650e7a
Your Name <you@example.com>
2024-06-27 17:59:03 +0700

    praktikum modul 10

---
+++ pertemuan10/admin/connect.php
@@ -0,0 +1,14 @@
+</div>
+  <div class="modal-footer">
+    <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+    <button type="button" class="btn btn-danger">Hapus</button>
+  </div>
+</div>
+</div>
+</div>
+<!-- Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+</body>
+</html>
